d8be16c6c2e0630d3c69f2659a70c0d4af761c22
atharavmishra <68751866+atharavmishra@users.noreply.github.com>
2022-12-16 19:02:51 +0530

    Committin all the files

--- app/src/main/java/android/myapplication/myapplication/RecipeActivity2.java
+++ app/src/main/java/android/myapplication/myapplication/RecipeActivity2.java
@@ -2,12 +2,16 @@ package android.myapplication.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.myapplication.modelClass.AnalyzedInstruction;
 import android.myapplication.modelClass.Result;
 import android.os.Bundle;
 import android.text.Html;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
@@ -15,6 +19,8 @@ public class RecipeActivity2 extends AppCompatActivity {
     ArrayList<Result> arraylist = new ArrayList<Result>();
     ArrayList <AnalyzedInstruction> Instructions = new ArrayList<AnalyzedInstruction>();
     TextView textview ;
+    ImageView imageView;
+    TextView name;
 
 
 
@@ -24,9 +30,14 @@ public class RecipeActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_recipe2);
         Intent intent = getIntent();
         textview = findViewById(R.id.text);
+        imageView = findViewById(R.id.image);
+        name = findViewById(R.id.name);
         arraylist=intent.getParcelableArrayListExtra("Data");
         int pos = intent.getIntExtra("position", 0);
         textview.setText(Html.fromHtml(Html.fromHtml(arraylist.get(pos).getSummary()).toString()));
+        Glide.with(this).load(arraylist.get(pos).getImage()).into(imageView);
+        name.setText(arraylist.get(pos).getTitle().toString());
+
 
 
     }
