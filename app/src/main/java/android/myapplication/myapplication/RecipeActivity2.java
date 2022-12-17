fe09d231d89ecc47fe7e1ee8e0275b60c52c8968
atharavmishra <68751866+atharavmishra@users.noreply.github.com>
2022-12-17 14:41:47 +0530

    Committin all the files

--- app/src/main/java/android/myapplication/myapplication/RecipeActivity2.java
+++ app/src/main/java/android/myapplication/myapplication/RecipeActivity2.java
@@ -4,14 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.fonts.Font;
 import android.myapplication.modelClass.AnalyzedInstruction;
 import android.myapplication.modelClass.Result;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -21,7 +25,9 @@ public class RecipeActivity2 extends AppCompatActivity {
     TextView textview ;
     ImageView imageView;
     TextView name;
-    String ingredients ="";
+    TextView ing_list;
+    Font font;
+    String ingredients ="Ingredients required for below step\n";
 
 
     @Override
@@ -32,16 +38,30 @@ public class RecipeActivity2 extends AppCompatActivity {
         textview = findViewById(R.id.text);
         imageView = findViewById(R.id.image);
         name = findViewById(R.id.name);
+        ing_list= findViewById(R.id.ingredients);
         arraylist=intent.getParcelableArrayListExtra("Data");
         int pos = intent.getIntExtra("position", 0);
         Instructions = arraylist.get(pos).getAnalyzedInstructions();
         textview.setText(Html.fromHtml(Html.fromHtml(arraylist.get(pos).getSummary()).toString()));
         Glide.with(this).load(arraylist.get(pos).getImage()).into(imageView);
         name.setText(arraylist.get(pos).getTitle().toString());
-        for(int i=0; i<=Instructions.get(0).getSteps().size(); i++){
-            for(int j=0; j<=Instructions.get(0).getSteps().get(i).getIngredients().size(); j++) {
-                ingredients = ingredients + Instructions.get(0).getSteps().get(i).getIngredients().get(j);
+        if(Instructions.size()==0){
+            ing_list.setText("Sorry There are no steps available");
+        }
+        else {
+            for (int i = 0; i < Instructions.get(0).getSteps().size(); i++) {
+                for (int j = 0; j < Instructions.get(0).getSteps().get(i).getIngredients().size(); j++) {
+
+                    ingredients = ingredients + Instructions.get(0).getSteps().get(i).getIngredients().get(j).getName() + ",";
+                    Log.d("Tag", String.valueOf(i));
+                    Log.d("Tag", String.valueOf(j));
+
+                }
+                ingredients = ingredients + "\n\nStep" + String.valueOf(i+1) +" - "+ Instructions.get(0).getSteps().get(i).getStep() + "\n\n\nIngredients Required for below step\n";
+
             }
+
+            ing_list.setText(ingredients);
         }
 
 
